2ff72863e98159daea004e1336a53f2516287f0e
fakturk <f.akturk@gmail.com>
2018-01-15 23:19:45 +0900

    small changes

--- mqtt.go
+++ mqtt.go
@@ -9,7 +9,7 @@ import (
 )
 
 //define a function for the default message handler
-var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+var publishHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
   fmt.Printf("TOPIC: %s\n", msg.Topic())
   fmt.Printf("MSG: %s\n", msg.Payload())
 }
@@ -19,17 +19,17 @@ func main() {
   //off trace output and set the default message handler
   opts := MQTT.NewClientOptions().AddBroker("tcp://iot.eclipse.org:1883")
   opts.SetClientID("go-simple")
-  opts.SetDefaultPublishHandler(f)
+  opts.SetDefaultPublishHandler(publishHandler)
 
   //create and start a client using the above ClientOptions
-  c := MQTT.NewClient(opts)
-  if token := c.Connect(); token.Wait() && token.Error() != nil {
+  client := MQTT.NewClient(opts)
+  if token := client.Connect(); token.Wait() && token.Error() != nil {
     panic(token.Error())
   }
 
   //subscribe to the topic /go-mqtt/sample and request messages to be delivered
   //at a maximum qos of zero, wait for the receipt to confirm the subscription
-  if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
+  if token := client.Subscribe("go-mqtt/ideafield", 0, nil); token.Wait() && token.Error() != nil {
     fmt.Println(token.Error())
     os.Exit(1)
   }
@@ -38,17 +38,17 @@ func main() {
   //from the server after sending each message
   for i := 0; i < 5; i++ {
     text := fmt.Sprintf("this is msg #%d!", i)
-    token := c.Publish("go-mqtt/sample", 0, false, text)
+    token := client.Publish("go-mqtt/ideafield", 0, false, text)
     token.Wait()
   }
 
   time.Sleep(3 * time.Second)
 
   //unsubscribe from /go-mqtt/sample
-  if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
+  if token := client.Unsubscribe("go-mqtt/ideafield"); token.Wait() && token.Error() != nil {
     fmt.Println(token.Error())
     os.Exit(1)
   }
 
-  c.Disconnect(250)
+  client.Disconnect(250)
 }
